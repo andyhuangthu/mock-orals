fc7544401729edf30e2f755c26a56d626c4ac7fa
andyhuangthu <andyhuangthu@gmail.com>
2025-07-26 11:30:53 -0700

    Update script.js - testing picking number of minutes

--- script.js
+++ script.js
@@ -110,11 +110,11 @@
                 attempt, words, wpm;
             for (attempt = 0; attempt < 1000000; attempt++) {
                 // Pick a random number of passages, anywhere between 6 to 12.
-                const minPassages = 6;
-                const maxPassages = 12;
+                // const minPassages = 6;
+                // const maxPassages = 12;
                 // Generate a random integer between minPassages and maxPassages, inclusive.
-                let numberOfPassagesToPick = Math.floor(Math.random() * (maxPassages - minPassages + 1)) + minPassages;
-                let picked = pick(passages, numberOfPassagesToPick);
+                // let numberOfPassagesToPick = Math.floor(Math.random() * (maxPassages - minPassages + 1)) + minPassages;
+                let picked = pick(passages, 12);
                 words = picked.reduce((total, p) => (total + p.word_count), 0);
                 wpm = words / minutes;
                 if (min_wpm <= wpm && wpm <= max_wpm) {
